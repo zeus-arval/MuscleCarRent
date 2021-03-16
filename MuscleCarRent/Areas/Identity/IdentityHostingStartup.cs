acd9a4b5eb242ae46d32435910ac7bdcb6cb9640
zeus-arval <72491384+zeus-arval@users.noreply.github.com>
2021-03-16 14:59:41 +0200

    Identity added

---
+++ MuscleCarRent/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MuscleCarRent.Data;
+
+[assembly: HostingStartup(typeof(MuscleCarRent.Areas.Identity.IdentityHostingStartup))]
+namespace MuscleCarRent.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MuscleCarRentContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MuscleCarRentContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MuscleCarRentContext>();
+            });
+        }
+    }
+}
